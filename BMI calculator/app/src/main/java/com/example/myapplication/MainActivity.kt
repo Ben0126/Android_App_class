5d6ddd76d072f0578d93946ec98626950c00c901
Ben Yeh <75458848+Ben0126@users.noreply.github.com>
2022-10-11 01:12:52 +0800

    BMI Intent version

--- BMI calculator/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ BMI calculator/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.pow
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +25,35 @@ class MainActivity : AppCompatActivity() {
         val height_input = findViewById<EditText>(R.id.editText_height)
         val button = findViewById<Button>(R.id.button)
         val outPut_text = findViewById<TextView>(R.id.output)
-        //val bmi = weight_input.text.toString().toDouble()/height_input.text.toString().toDouble().pow(2)
-        //val bmi_round = String.format("%.2f", bmi).toDouble()
+
+        val getResult =
+            registerForActivityResult(
+                ActivityResultContracts
+                    .StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val Gender = it.data?.getStringExtra("BMI_suggest")
+//                    val GenderFloat = Gender?.toFloat()
+                    outPut_text.text = Gender
+//                    if (GenderFloat != null) {
+//                        outPut_text.text = "${ if (GenderFloat<18.5)  "過輕" else (if(GenderFloat<24) "正常" else "過重")   }"
+
+//                    }
+                }
+            }
+
         button.setOnClickListener{
+
             val bmi = weight_input.text.toString().toDouble()/height_input.text.toString().toDouble().pow(2)
             val bmi_round = String.format("%.2f", bmi).toDouble()
-            outPut_text.text = if (weight_input.text.isNotEmpty() && height_input.text.isNotEmpty() && name_input.text.isNotEmpty())
-                "${name_input.text.toString()} 的 BMI:\n${bmi_round.toString()}" +
-                        " (${if ((bmi_round < 18.5)) "過輕" 
-                        else (if (bmi_round > 24) "過重" 
-                        else "正常")})"
-            else "result:\n請輸入正確"
+
+
+            val BMI = if (weight_input.text.isNotEmpty() && height_input.text.isNotEmpty() && name_input.text.isNotEmpty())
+                        "${bmi_round.toString()}"
+                else "result:\n請輸入正確"
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("BMI_EXTRA", BMI);
+            getResult.launch(intent)
         }
     }
 
